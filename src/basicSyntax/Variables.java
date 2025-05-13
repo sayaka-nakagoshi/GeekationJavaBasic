f4835932a1913392d81bc96d2571746f6ae87bb6
sayakanakagoshi <sayakanakagoshi@192.168.3.4>
2025-05-13 22:36:15 +0900

    変数課題提出

---
+++ src/basicSyntax/Variables.java
@@ -0,0 +1,16 @@
+package basicSyntax;
+
+public class Variables {
+
+    public static void main(String[] args) {
+      String myFavorite = "私の好きな";
+      int num1 = 3;
+      double num2 = 3.14;
+      
+      System.out.println(num1);
+      System.out.println(num2);
+      System.out.println(myFavorite + "整数は" + num1 + "です。");
+      System.out.println(myFavorite + "小数は" + num2 + "です。");
+      
+    }
+}
